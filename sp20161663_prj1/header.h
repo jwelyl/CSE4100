b972d940350f56146246caf740eb253a2c0e22c9
undergraduate <cse20161663@cspro.sogang.ac.kr>
2021-03-10 05:39:41 -0800

    기본적인 인터페이스, help, dir, quit, history 구현

--- sp20161663_prj1/header.h
+++ sp20161663_prj1/header.h
@@ -5,7 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
-char memory[1048577];
+#define MAX_MEM_SIZE 1048577
+#define CMD_LEN           11
+#define MNEMONIC           7
+
+char memory[MAX_MEM_SIZE];
+
+//  operands
+char mnemonic[MNEMONIC];
+int dstart, dend;
+int eaddress, evalue;
+int fstart, fend, fvalue;
 
 #endif
